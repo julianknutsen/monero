ba055982da43ec9272ed4393082de08eb3d4737a
Julian Knutsen <dev@julianknutsen.com>
2017-12-13 12:39:25 -0800

    Add basic travis-ci config

---
+++ travis/install_deps.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+sudo apt-get update && sudo apt-get install -y build-essential cmake pkg-config libboost-all-dev libssl-dev libzmq3-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libreadline6-dev libldns-dev libexpat1-dev doxygen graphviz
